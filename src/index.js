f279f3b36554b679dd9f9c2ea10d04bce9e18d0f
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-28 12:32:33 +0200

    La til eksempåler på bruk av todos og prosjekter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import CreateTodos from './scripts/CreateTodos.js';
+import Project from "./scripts/Projects.js";
+
+const project = new Project('skole')
+// Opprett en ny instans av klassen
+const myTodo = new CreateTodos('Dette','gjøre ferdifg','02/10/2025','');
+
+
+project.addTodo(myTodo)
+console.log(project.showTodos()); 
