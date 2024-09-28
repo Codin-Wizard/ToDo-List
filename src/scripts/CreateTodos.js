16ed588778550687a8e85a43407b80d2b7a9164d
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-09-28 12:31:45 +0200

    Et start for hvordan jeg skal lage todos

--- src/scripts/CreateTodos.js
+++ src/scripts/CreateTodos.js
@@ -1,7 +1,9 @@
 export default class CreateTodos {
-    constructor(title, description) {
+    constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority
     }
 
     text() {
@@ -9,6 +11,22 @@ export default class CreateTodos {
         const description = this.description;
 
         return `The title is ${title}, and text is ${description}`;
+    }
+
+    CalcDueDate() {
+        const currentDay = new Date(); //Dagens dato
+        const dueDate = new Date(this.dueDate); //Antar at duedate er i framtiden
+
+        // Beregn differansen i millisekunder mellom forfallsdato og dagens dato
+        const differenceInTime = dueDate - currentDay;
+
+        const differenceInDays = Math.ceil(differenceInTime / (1000 * 60 * 60 * 24));
+
+        return `Forfallsdato er om ${differenceInDays} dager.`;
+    }
+
+    getPriority() {
+        
     }
 }
 
